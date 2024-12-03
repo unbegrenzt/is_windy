adee5dcd733a46ffff5744cd9e0a62efae5ea2bb
unbegrenzt <jmoralescenteno@gmail.com>
2024-12-03 12:25:14 -0600

    refactor: :rocket: send new relase

--- src/components/molecules/SearchBar/index.tsx
+++ src/components/molecules/SearchBar/index.tsx
@@ -3,11 +3,11 @@
 import React from 'react';
 import Input from '@/components/atoms/Input';
 import ResultsList from '@/components/atoms/ResultsList';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import useSearchOSM from '@/hooks/useSearchOSM';
-import Player from 'lottie-react';
-import loadingAnimation from '@/animations/loading.json';
+// import Player from 'lottie-react';
+// import loadingAnimation from '@/animations/loading.json';
 
 const SearchBar: React.FC = () => {
   const [query, setQuery] = React.useState('');
@@ -16,7 +16,7 @@ const SearchBar: React.FC = () => {
   return (
     <div className="relative w-3/4 lg:max-w-96">
       <div className="flex items-center rounded-full border border-gray-300 p-2.5">
-        {loading ? (
+        {/* {loading ? (
           <Player
             autoplay
             loop
@@ -25,7 +25,7 @@ const SearchBar: React.FC = () => {
           />
         ) : (
           <FontAwesomeIcon icon={faSearch} className="mr-2" />
-        )}
+        )} */}
         <Input
           type="text"
           placeholder="Search..."
